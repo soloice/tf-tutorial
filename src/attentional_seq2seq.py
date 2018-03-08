a227c27abe69f27321be132c405e6b2c7921fe8d
lurker <totentanz@pku.edu.cn>
2018-03-08 23:24:19 +0800

    ADD: visualize attention matrix.

--- src/attentional_seq2seq.py
+++ src/attentional_seq2seq.py
@@ -2,21 +2,40 @@ import tensorflow as tf
 import numpy as np
 import random
 import os
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+def plot_align_matrix(src, tgt, matrix):
+    src = [str(item) for item in src]
+    tgt = [str(item) for item in tgt]
+    df = pd.DataFrame(matrix, index=src, columns=tgt)
+    ax = sns.heatmap(df, linewidths=.5)
+    ax.set_xlabel("target")
+    ax.set_ylabel("source")
+    plt.xticks(rotation=90)
+    plt.yticks(rotation=0)
+    ax.set_title("alignment heatmap")
+    plt.show()
+    return matrix
+
 
 vocab_size = 10 + 1 # 1~10 + 0
 max_len = 24
+MAX_DECODE_STEP = max_len
 batch_size = 64
 PAD = 0
 EOS = 0
 GO = 0
-MAX_DECODE_STEP = 2 * max_len
+
 
 odd_list, even_list = [1, 3, 5, 7, 9] * 10, [2, 4, 6, 8, 10] * 10
 
 
 def generate_data(num_samples=batch_size):
     num_odds = np.random.randint(low=1, high=max_len//2, size=num_samples)
-    num_evens = np.random.randint(low=1, high=max_len // 2, size=num_samples)
+    num_evens = np.random.randint(low=1, high=max_len//2, size=num_samples)
     batch_len_x = num_odds + num_evens
     batch_len_y = num_evens + 1  # <EOS>/<GO>
 
@@ -58,6 +77,7 @@ if not os.path.exists(summary_path):
 
 encoder_embedding_size, decoder_embedding_size = 30, 30
 encoder_hidden_units, decoder_hidden_units = 50, 50
+attention_depth = decoder_hidden_units
 encoder_lstm_layers, decoder_lstm_layers = 2, 2
 
 # [B, T]
@@ -85,7 +105,7 @@ with tf.variable_scope("encoder"):
                       for _ in range(encoder_lstm_layers)]
     encoder = tf.contrib.rnn.MultiRNNCell(encoder_layers)
 
-    _, encoder_final_state = tf.nn.dynamic_rnn(
+    encoder_all_outputs, encoder_final_state = tf.nn.dynamic_rnn(
         encoder, encoder_inputs_embedded,
         sequence_length=encoder_length,
         dtype=tf.float32, time_major=False, scope="seq2seq_encoder")
@@ -96,18 +116,38 @@ with tf.variable_scope("decoder"):
     decoder_layers = [tf.contrib.rnn.BasicLSTMCell(encoder_hidden_units)
                       for _ in range(decoder_lstm_layers)]
     decoder = tf.contrib.rnn.MultiRNNCell(decoder_layers)
+
+    attention_mechanism = tf.contrib.seq2seq.LuongAttention(
+        num_units=attention_depth,
+        memory=encoder_all_outputs,
+        memory_sequence_length=encoder_length)
+
+    attn_decoder = tf.contrib.seq2seq.AttentionWrapper(
+        decoder, attention_mechanism,
+        # cell_input_fn=lambda inputs, attention: inputs,
+        alignment_history=True, output_attention=True)
+
+
     fc_layer = tf.layers.Dense(vocab_size)
 
     training_helper = tf.contrib.seq2seq.TrainingHelper(decoder_inputs_embedded,
                                                         decoder_length)
-    training_decoder = tf.contrib.seq2seq.BasicDecoder(decoder, training_helper,
-                                                       encoder_final_state, fc_layer)
+
+    decoder_initial_state = attn_decoder.zero_state(batch_size, tf.float32).clone(
+        cell_state=encoder_final_state)
+
+    training_decoder = tf.contrib.seq2seq.BasicDecoder(
+        cell=attn_decoder, helper=training_helper,
+        initial_state=decoder_initial_state, output_layer=fc_layer)
 
     logits, final_state, final_sequence_lengths = \
         tf.contrib.seq2seq.dynamic_decode(training_decoder)
 
     # decoder_logits: [B, T, V]
     decoder_logits = logits.rnn_output
+    # [decoder_steps, batch_size, encoder_steps]
+    attention_matrices = final_state.alignment_history.stack(
+        name="train_attention_matrix")
     print("logits: ", decoder_logits)
 
 
@@ -130,13 +170,21 @@ start_tokens = tf.tile([GO], [num_sequences_to_decode])
 inference_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(
     decoder_embedding_matrix, start_tokens, end_token=EOS)
 
-greedy_decoder = tf.contrib.seq2seq.BasicDecoder(
-    cell=decoder, helper=inference_helper,
-    initial_state=encoder_final_state, output_layer=fc_layer)
+inference_decoder_initial_state = attn_decoder.zero_state(
+    num_sequences_to_decode, tf.float32).clone(
+    cell_state=encoder_final_state)
 
-greedy_decoding_result, _1, _2 = tf.contrib.seq2seq.dynamic_decode(
+greedy_decoder = tf.contrib.seq2seq.BasicDecoder(
+    cell=attn_decoder, helper=inference_helper,
+    initial_state=inference_decoder_initial_state, output_layer=fc_layer)
+
+greedy_decoding_result, greedy_final_state, _2 = tf.contrib.seq2seq.dynamic_decode(
     decoder=greedy_decoder, output_time_major=False,
     impute_finished=True, maximum_iterations=MAX_DECODE_STEP)
+
+# [decoder_steps, batch_size, encoder_steps]
+inference_attention_matrices = greedy_final_state.alignment_history.stack(
+    name="inference_attention_matrix")
 
 
 def get_decoder_input_and_output(ids):
@@ -154,19 +202,37 @@ def draw_samples(session, number_samples_to_draw=3):
     feed = {encoder_inputs: x,
             encoder_length: lx,
             num_sequences_to_decode: number_samples_to_draw}
-    greedy_prediction = session.run(greedy_decoding_result,
-                                 feed_dict=feed)
+    greedy_prediction, att_mat = session.run(
+        [greedy_decoding_result, inference_attention_matrices], feed_dict=feed)
+
+    np.set_printoptions(precision=2, linewidth=1000)
     print("=" * 100)
     print("Sample x:")
     print(x)
     print("Expected y:")
     print(y)
     print("Greedy Decoding result:")
-    print(greedy_prediction.sample_id)
+    y_ = greedy_prediction.sample_id
+    print(y_)
+
+    # 随机取一个样本 i 画出注意力矩阵
+    i = np.random.randint(low=1, high=number_samples_to_draw)
+    # matrix[k, l] -> encoder step `k` vs. decoder step `l`
+    matrix = att_mat[:, i, :].T
+    y_len = y_.shape[1]
+    for idx in range(y_.shape[1]):
+        if y_[i, idx] == 0:
+            y_len = idx + 1
+            break
+    x_valid = x[i, :lx[i]]
+    y_valid = y_[i, :y_len]
+    # print(x_valid)
+    # print(y_valid)
+    plot_align_matrix(src=x_valid, tgt=y_valid,
+                      matrix=matrix[:lx[i], :y_len])
 
 
 max_batches = 5001
-batches_in_epoch = 100
 save_period = 500
 
 saver = tf.train.Saver()
@@ -193,12 +259,11 @@ with tf.Session() as sess:
                 decoder_targets: y_out,
                 encoder_length: lx,
                 decoder_length: ly}
-        _, loss_ = sess.run([train_op, loss], feed_dict=feed)
+        _, loss_, att = sess.run([train_op, loss, attention_matrices], feed_dict=feed)
+        # print(att.shape, max(lx), max(ly))
 
         if batch_id % save_period == 0:
-            saver.save(sess, save_path=model_name, global_step=batch_id)
-
-        if batch_id % batches_in_epoch == 0:
+            # saver.save(sess, save_path=model_name, global_step=batch_id)
             print('batch {}'.format(batch_id))
             print('  minibatch loss: {}'.format(loss_))
             draw_samples(session=sess)
